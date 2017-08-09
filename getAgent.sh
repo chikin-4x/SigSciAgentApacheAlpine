cf8c65d00c1e5f6267bc4c9dc930dbab6046474b
Douglas Coburn <douglas@dactbc.com>
2017-08-08 20:24:07 -0700

    Fixing git merge issue, missed a file

--- getAgent.sh
+++ getAgent.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 
-<<<<<<< HEAD
+
 #Get Current agent
-=======
->>>>>>> 0abb64ef722deb32eeb8edfcef80049348ed5ec6
+
 curl https://dl.signalsciences.net/sigsci-agent/sigsci-agent_latest.tar.gz > sigsci-agent_latest.tar.gz
 rm -rf contrib/sigsci-agent/*
 cd contrib/sigsci-agent
 tar -zxvf ../../sigsci-agent_latest.tar.gz
 cd ../../
 rm -rf sigsci-agent_latest.tar.gz
-<<<<<<< HEAD
-
-=======
->>>>>>> 0abb64ef722deb32eeb8edfcef80049348ed5ec6
